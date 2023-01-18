65f8669aeb7c0f3e5aa885b0164809a70e578cb4
Gusb <mihivanmih@gmail.com>
2023-01-18 18:33:19 +0300

    Parallax

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    //direction: 'vertical',
+    //freeMode: true,
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 20
+    },
+    spaceBetween: 18,
+    parallax: true,
+})
